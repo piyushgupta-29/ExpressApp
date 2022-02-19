5fddedf49de895fd0a2cb689341f6f979f42b760
piyushgupta-29 <piyushgupta2001.29@gmail.com>
2022-02-19 21:04:09 +0530

    new commit

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const dotenv = require("dotenv");
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 dotenv.config({ path: './.env' });
 
 require('./db/conn');
 
+app.use(cors());
+
 app.use(express.json());
+
 app.use(require('./routes/auth'));
 
 const PORT = 8000;
