1175e68cb3d3be6cc2c7a64066ae7ca7c0e16288
piyushgupta-29 <piyushgupta2001.29@gmail.com>
2022-02-19 20:26:56 +0530

    new commit

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const dotenv = require("dotenv");
 const express = require('express');
-const path = require('path');
 const app = express();
 
 dotenv.config({ path: './.env' });
@@ -8,12 +7,10 @@ dotenv.config({ path: './.env' });
 require('./db/conn');
 
 app.use(express.json());
-// app.use(express.static(path.join(__dirname,'/views')));
-// app.set('view engine','ejs');
 app.use(require('./routes/auth'));
 
-const PORT = process.env.PORT;
+const PORT = 8000;
 
 app.listen(PORT, () => {
-    console.log(`server is runnig at port no ${PORT}`);
+    console.log(`server is running at port no ${PORT}`);
 })
